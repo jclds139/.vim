b3ed4e8a2f6154dbef0d79cac68fceba78e24c3d
Jesse R Codling <codling@umich.edu>
2025-06-17 21:50:05 -0400

    fixup! actually use pcall() to try and start treesitter

--- ts.lua
+++ ts.lua
@@ -18,11 +18,9 @@ require 'nvim-treesitter'.install({
 
 vim.api.nvim_create_autocmd({ "FileType", "BufEnter", "BufWinEnter" }, {
 	callback = function()
-		ts_lang = vim.treesitter.language.get_lang(vim.bo.filetype)
-		pcall(require'nvim-treesitter'.install(ts_lang))
-		local found, _ = pcall(vim.treesitter.language.inspect(ts_lang))
-		if found then
-			vim.treesitter.start(0)
+		local ts_lang = vim.treesitter.language.get_lang(vim.bo.filetype)
+		pcall(require'nvim-treesitter'.install, ts_lang)
+		if pcall(vim.treesitter.start, 0) then
 			vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
 		end
